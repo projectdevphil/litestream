cc6a38008ecbce6f4ceda6f6a1783734cb989215
Dangerouxxx <arbiemapalopeligro@gmail.com>
2025-12-06 23:59:21 +0800

    Update getTopChannels.js

--- api/getTopChannels.js
+++ api/getTopChannels.js
@@ -7,8 +7,8 @@ export const config = {
 export default async function handler(request) {
   try {
     const redis = new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL || process.env.STORAGE_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN || process.env.STORAGE_TOKEN,
+      url: process.env.UPSTASH_REDIS_REST_KV_REST_API_URL,
+      token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN,
     });
 
     const topChannels = await redis.zrange('global_top_watch', 0, 4, { rev: true });
